597fa5ea82864981b8350a879367a43f30caf1c1
Oleg Shevchenko <bloodunit@gmail.com>
2013-07-15 12:07:58 +0400

    UI bugfixes

--- data/scripts/widget.js
+++ data/scripts/widget.js
@@ -1,18 +1,23 @@
+var widgetGlobal = {
+    icons: {
+        default: "images/icon.png",
+        inactive: "images/icon_inactive.png"
+    }
+}
+
 self.port.on("onFeedsUpdate", function(data){
-
-    //TODO: Set properly icon
+    var image = document.getElementById("image");
     if(data.isLoggedIn){
-
+        image.src = widgetGlobal.icons.default;
     }else{
-
+        image.src = widgetGlobal.icons.inactive;
     }
 
     var counter = document.getElementById("counter");
     if(data.unreadFeedsCount > 0){
         counter.innerHTML = data.unreadFeedsCount;
     }else{
-        //For test only
-        counter.innerHTML = 0;
+        counter.innerHTML = "";
     }
 });
 
