c03cc60999249c82514b4ee690969ff1be904d06
olsh <bloodunit@gmail.com>
2013-10-02 17:02:08 +0400

    Added sandbox task in Gruntjs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,15 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        copy: {
+            main: {
+                files: [
+                    {expand: true, cwd: '<%= pkg.sourcePath %>/', src: ['**'], dest: '<%= pkg.buildPath %>/'}
+                ]
+            }
+        },
         "string-replace": {
-            dist: {
+            keys: {
                 files: {
                     "<%= pkg.buildPath %>/lib/main.js": "<%= pkg.buildPath %>/lib/main.js"
                 },
@@ -10,11 +17,37 @@ module.exports = function (grunt) {
                     replacements: [
                         {
                             pattern: 'clientSecret: ""',
-                            replacement: 'clientSecret: "' + grunt.option("clientSecret")  +  '"'
+                            replacement: 'clientSecret: "' + grunt.option("clientSecret") + '"'
                         },
                         {
                             pattern: 'clientId: ""',
-                            replacement: 'clientId: "' + grunt.option("clientId")  +  '"'
+                            replacement: 'clientId: "' + grunt.option("clientId") + '"'
+                        }
+                    ]
+                }
+            },
+            sandboxApi: {
+                files: {
+                    "<%= pkg.buildPath %>/lib/feedly.api.js": "<%= pkg.buildPath %>/lib/feedly.api.js"
+                },
+                options: {
+                    replacements: [
+                        {
+                            pattern: /http(?:s)?:\/\/(?:www\.)?cloud\.feedly\.com/gi,
+                            replacement: "http://sandbox.feedly.com"
+                        }
+                    ]
+                }
+            },
+            sandboxLink: {
+                files: {
+                    "<%= pkg.buildPath %>/data/popup.html": "<%= pkg.buildPath %>/data/popup.html"
+                },
+                options: {
+                    replacements: [
+                        {
+                            pattern: /http(?:s)?:\/\/(?:www\.)?feedly\.com/gi,
+                            replacement: "http://sandbox.feedly.com"
                         }
                     ]
                 }
@@ -26,28 +59,44 @@ module.exports = function (grunt) {
                     join_vars: false
                 }
             },
-            dist: {
+            build: {
                 files: {
-                    "<%= pkg.buildPath %>/lib/main.js" : ["<%= pkg.buildPath %>/lib/main.js"],
-                    "<%= pkg.buildPath %>/lib/feedly.api.js" : ["<%= pkg.buildPath %>/lib/feedly.api.js"],
-                    "<%= pkg.buildPath %>/data/scripts/popup.js" : ["<%= pkg.buildPath %>/data/scripts/popup.js"],
-                    "<%= pkg.buildPath %>/data/scripts/widget.js" : ["<%= pkg.buildPath %>/data/scripts/widget.js"]
+                    "<%= pkg.buildPath %>/lib/main.js": ["<%= pkg.buildPath %>/lib/main.js"],
+                    "<%= pkg.buildPath %>/lib/feedly.api.js": ["<%= pkg.buildPath %>/lib/feedly.api.js"],
+                    "<%= pkg.buildPath %>/data/scripts/popup.js": ["<%= pkg.buildPath %>/data/scripts/popup.js"],
+                    "<%= pkg.buildPath %>/data/scripts/widget.js": ["<%= pkg.buildPath %>/data/scripts/widget.js"]
                 }
             }
         },
-        copy: {
-            main: {
+        "mozilla-addon-sdk": {
+            download: {
+                options: {
+                    revision: "1.14"
+                }
+            },
+            xpi: {
+                options: {
+                    extension_dir: "<%= pkg.buildPath %>",
+                    dist_dir: "<%= pkg.buildPath %>"
+                }
+            }
+        },
+        clean: {
+            build: {
                 files: [
-                    {expand: true, cwd: '<%= pkg.sourcePath %>/', src: ['**'], dest: '<%= pkg.buildPath %>/'}
+                    {expand: true, cwd: "<%= pkg.buildPath %>", src: ["*", "!*.xpi"]}
                 ]
             }
         }
     });
 
+    grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks('grunt-string-replace');
     grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks('grunt-mozilla-addon-sdk');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask("default", ["copy", "string-replace", "uglify"]);
-    grunt.registerTask("develop", ["copy", "string-replace"]);
+    grunt.registerTask("build", ["copy", "string-replace:keys", "uglify", "mozilla-addon-sdk", "clean"]);
+    grunt.registerTask("sandbox", ["copy", "string-replace"]);
+    grunt.registerTask("default", ["copy", "string-replace:keys"]);
 };
