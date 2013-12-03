e4c69dd0d56f6b84bf77a2c6e9c87fecc1ecb48e
Oleg Shevchenko <bloodunit@gmail.com>
2013-12-03 10:08:59 +0400

    Uglify was removed, version changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,21 +53,6 @@ module.exports = function (grunt) {
                 }
             }
         },
-        uglify: {
-            options: {
-                compress: {
-                    join_vars: false
-                }
-            },
-            build: {
-                files: {
-                    "<%= pkg.buildPath %>/lib/main.js": ["<%= pkg.buildPath %>/lib/main.js"],
-                    "<%= pkg.buildPath %>/lib/feedly.api.js": ["<%= pkg.buildPath %>/lib/feedly.api.js"],
-                    "<%= pkg.buildPath %>/data/scripts/popup.js": ["<%= pkg.buildPath %>/data/scripts/popup.js"],
-                    "<%= pkg.buildPath %>/data/scripts/widget.js": ["<%= pkg.buildPath %>/data/scripts/widget.js"]
-                }
-            }
-        },
         "mozilla-addon-sdk": {
             stable: {
                 options: {
@@ -105,11 +90,10 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks('grunt-string-replace');
-    grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks('grunt-mozilla-addon-sdk');
     grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask("build", ["copy", "string-replace:keys", "uglify", "mozilla-addon-sdk", "mozilla-cfx-xpi", "clean"]);
+    grunt.registerTask("build", ["copy", "string-replace:keys", "mozilla-addon-sdk", "mozilla-cfx-xpi", "clean"]);
     grunt.registerTask("sandbox", ["copy", "string-replace", "mozilla-addon-sdk", "mozilla-cfx"]);
     grunt.registerTask("default", ["copy", "string-replace:keys", "mozilla-addon-sdk", "mozilla-cfx"]);
 };
